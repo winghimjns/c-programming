1027d3dc0b57cdd4c268cba24d79b84a928ee29c
winghimjns <winghim_01@hotmail.com>
2022-06-11 15:41:15 +0800

    chore(tutorials): submitted two more tutorials: 03-types and 04-printf

---
+++ 03-types/sources/main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+int main()
+{
+	int age = 40;
+	double gpa = 40.0;
+	char grade = 'A';
+	char phrase[] = "Giraffe Academy";
+
+	return 0;
+}
---
+++ 04-printf/sources/main.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main ()
+{
+	printf("My favourite %s is %d\n", "number", 500);
+	return 0;
+}
