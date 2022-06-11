99d95ae78762e096701c758f40a5a3d32e53327d
winghimjns <winghim_01@hotmail.com>
2022-06-11 15:18:21 +0800

    chore(variables): done the second tutorial: variables and printf's string format functionality

---
+++ variables/sources/main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	char characterName[] = "John";
+	int characterAge = 35;
+	printf("There once was a man named %s.\n", characterName);
+	printf("he was %d years old.\n", characterAge);
+	printf("He really liked the name %s.\n", characterName);
+	printf("but did not like being %d.\n", characterAge);
+}
