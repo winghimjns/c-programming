488e5ed5f07bedb0ce36a4eafeadeedc08fe8476
winghimjns <winghim_01@hotmail.com>
2022-06-12 20:46:45 +0800

    chore(number): updated lesson 5

--- 05-numbers/sources/main.c
+++ 05-numbers/sources/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main ()
 {
 	printf("%f\n", 5 + 4.5);
+	printf("%f\n", pow(2, 3));
+	printf("%f\n", sqrt(36));
+	printf("%f\n", ceil(36.7));
+	printf("%f\n", floor(37.7));
+	printf("%f\n", round(37.7));
 	return 0;
 }
